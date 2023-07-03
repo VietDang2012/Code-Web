5b2785a64d1ce33b5975c61bc5e5f30f3f8edb0b
vietdang <dangtheviet2012@gmail.com>
2023-07-03 21:13:27 +0700

    Add main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+
+// Đặt biến
+var fullName = 'Dang The Viet'
+var age = 26
+
+// Cửa số cảnh báo
+alert(fullName)
+alert(age)
+
+// Viết ra trang web
+document.write('Welcome to my site!')
+
+// In ra console
+console.log(fullName)
+console.warn(fullName)
+console.error(fullName)
+
+// Cửa sổ xác nhận
+confirm('Bạn có đủ 18 tuổi không ?')
+
+// Cửa sổ điền số xác nhận
+prompt('Bạn có đủ 18 tuổi không ?')
+
+// Toán tử
+var a = 1;
+var b = ++a * a--;
+
+// Hàm có sẵn
+setInterval(function() {
+    console.log(b)
+}, 1)
